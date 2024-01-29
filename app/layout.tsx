e88ae856c0628b4cd2c7c8c7a89c34b596ec819f
leonardo.videc <leonardo.videc@ecx.io>
2024-01-29 20:39:32 +0100

    initialized movie platform repository with next 14

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Movie platform",
+  description: "Movie platorm that lets you research and watch movies in great quality.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={cn(inter.className, "bg-slate-800 text-gray-300")}>{children}</body>
+    </html>
+  );
+}
---
+++ next.config.mjs
@@ -0,0 +1,24 @@
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+
+/** @type {import("next").NextConfig} */
+const config = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.justwatch.com',
+      },
+    ],
+  },
+};
+
+
+export default config;
